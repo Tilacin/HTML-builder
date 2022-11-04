2e2c0ca11b94964a2a7a8f915de5162fd3a3d9d6
Tilacin <tilacin45@gmail.com>
2022-11-04 21:27:40 +0300

    add: task 1

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+
+const fs = require('fs')
+fs.readFile(__dirname + '/text.txt', {flag: 'r+', encoding: 'utf8'}, function (err, data) {
+    if(err) {
+     console.error(err);
+     return;
+    }
+    console.log(data);
+});
+
+ 
