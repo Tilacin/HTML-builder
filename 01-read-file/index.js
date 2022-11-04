bdd16fdbd162b15515cd3681320bfa3d2e9ba6fd
Tilacin <tilacin45@gmail.com>
2022-11-04 21:57:36 +0300

    refactor: task 1

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,10 @@
-
-const fs = require('fs')
-fs.readFile(__dirname + '/text.txt', {flag: 'r+', encoding: 'utf8'}, function (err, data) {
-    if(err) {
-     console.error(err);
-     return;
-    }
-    console.log(data);
+const fs = require("fs");
+const path = require("path");
+const way = path.join(__dirname, "text.txt");
+fs.readFile(way, { flag: "r+", encoding: "utf8" }, function (err, data) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
-
- 
